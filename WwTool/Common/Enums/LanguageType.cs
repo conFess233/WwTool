e515f683adb33afd0a05682ddd2548ba92fd3aca
confess233 <kout5504@gmail.com>
2026-06-10 14:03:37 +0900

    docs: 更新卡池文本

--- WwTool/Common/Enums/LanguageType.cs
+++ WwTool/Common/Enums/LanguageType.cs
@@ -10,28 +10,28 @@ namespace WwTool.Common.Enums
         [Description("简体中文")]
         ZhHans,
 
-        [Description("繁体中文")]
-        ZhHant,
+        //[Description("繁体中文")]
+        //ZhHant,
 
         [Description("英语")]
         En,
 
         [Description("日语")]
-        Ja,
+        Ja
 
-        [Description("韩语")]
-        Ko,
+        //[Description("韩语")]
+        //Ko,
 
-        [Description("德语")]
-        De,
+        //[Description("德语")]
+        //De,
 
-        [Description("西班牙语")]
-        Es,
+        //[Description("西班牙语")]
+        //Es,
 
-        [Description("法语")]
-        Fr,
+        //[Description("法语")]
+        //Fr,
 
-        [Description("泰语")]
-        Th
+        //[Description("泰语")]
+        //Th
     }
 }
