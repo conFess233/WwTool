01f4896fa06488990a1d579bd55e9c30221eb661
confess233 <kout5504@gmail.com>
2026-06-20 14:52:31 +0900

    refactor: 重构 ObjectExtensions
    改为纯反射浅拷贝

--- WwTool/Extensions/ObjectExtensions.cs
+++ WwTool/Extensions/ObjectExtensions.cs
@@ -1,19 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System;
 
 namespace WwTool.Extensions
 {
     public static class ObjectExtensions
     {
-        public static void CopyTo<T>(this T source, T target)
+        public static void CopyTo<T>(this T source, T target) where T : class
         {
+            if (source == null || target == null) return;
+
             foreach (var prop in typeof(T).GetProperties())
             {
-                if (!prop.CanRead || !prop.CanWrite)
-                    continue;
-
-                prop.SetValue(target, prop.GetValue(source));
+                if (prop.CanRead && prop.CanWrite)
+                {
+                    try
+                    {
+                        prop.SetValue(target, prop.GetValue(source));
+                    }
+                    catch
+                    {
+                        // 忽略属性复制时的个别反射异常
+                    }
+                }
             }
         }
     }
